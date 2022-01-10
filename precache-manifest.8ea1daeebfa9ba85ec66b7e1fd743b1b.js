d06c27e78f759bb7ded40c57772459c422b2c852
Sunny Bhagat <bhagatsunny96@gmail.com>
2022-01-10 15:12:49 +0530

    Updates

---
+++ precache-manifest.8ea1daeebfa9ba85ec66b7e1fd743b1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e75e5c1bbc98e2687d74868a127d37c",
+    "url": "/Chess/index.html"
+  },
+  {
+    "revision": "c2e8025876c3ad590b19",
+    "url": "/Chess/static/css/main.099c09f6.chunk.css"
+  },
+  {
+    "revision": "2b6191a715d48e7678d7",
+    "url": "/Chess/static/js/2.283b51ac.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/Chess/static/js/2.283b51ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e8025876c3ad590b19",
+    "url": "/Chess/static/js/main.876d468e.chunk.js"
+  },
+  {
+    "revision": "15f703dd858504ba95ee",
+    "url": "/Chess/static/js/runtime-main.5e7b2f1b.js"
+  },
+  {
+    "revision": "3382a95341b72c229183dcc0f1168176",
+    "url": "/Chess/static/media/q_w.3382a953.png"
+  }
+]);
